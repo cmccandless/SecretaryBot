91d64eea24ce31d0595853444ae8107d48bf9ac4
cmccandless <crm1994@gmail.com>
2016-06-08 11:57:19 -0400

    Functional Commit

--- bot.py
+++ bot.py
@@ -1,16 +1,28 @@
-import praw,OAuth2Util
+import praw,OAuth2Util,time
 
-subreddit=''
+
+botName='FILL_IN'
+subreddit='FILL_IN'
 user_agent='Forwards all received PMs to approved submitters in /r/{}'.format(subreddit)
 
 r = praw.Reddit(user_agent)
 o = OAuth2Util.OAuth2Util(r)
 o.refresh(force=True)
 
-url=r.get_authorize_url('uniqueKey','identity',True)
-import webbrowser
-webbrowser.open(url)
+#UNCOMMENT TO REAUTHORIZE
+# url=r.get_authorize_url('uniqueKey','identity',True)
+# import webbrowser
+# webbrowser.open(url)
 
-while(True)
-  for m in r.get_unread(limit=None):
-    
+#UNCOMMENT AND INCREASE INDENT FOR ALL LINES BELOW TO RUN CONTINUOUSLY
+# while(True):
+submitters=[]
+for u in r.get_contributors(subreddit,limit=None):
+	submitters.append(str(u))
+for m in r.get_unread(limit=None):
+	print(vars(m.author))
+	print("a:{}s:{};m:{}".format(m.author.name,m.subject,m.message))
+	for recipient in submitters:
+		if recipient != m.author.name and recipient != botName:
+			r.send_message(recipient, m.subject, m.message)
+time.sleep(3600) #Wait for 10 minutes
